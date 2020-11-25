bd6bd06f0b46f2e71edde738ca2a10657339068f
pjbtc <pjbhattachan@gmail.com>
2020-11-25 20:34:01 +0000

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ReduxDemo from './ReduxDemo';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      
+        <ReduxDemo/>
+      </div>
+    );
+  }
+}
+
+export default App;
